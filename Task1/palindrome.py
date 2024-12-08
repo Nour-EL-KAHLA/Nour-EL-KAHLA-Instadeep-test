211116308c3ac22f111027aee1f65af08f7983e3
Nour EL KAHLA <62484447+Nour-EL-KAHLA@users.noreply.github.com>
2024-12-08 12:48:42 +0100

    modif Py

--- Task1/palindrome.py
+++ Task1/palindrome.py
@@ -12,15 +12,13 @@ def palindrome(s):
     t=True
      
     #then we checked if the string is palindrome or not
-    for i in range(0,l%2-1):
-        print(s[i])
-        print(s[l-i])
+   
         #the moment we know it's impossible we break and return false
-        if(s[i]!=s[l-i]):
-            t=False
-            break
+    if(s[i]==s[::-1]):
+        return True
     #else we return true
-    return t
+    else :
+        return False
 
 print(palindrome("Madam, in Eden, I'm Adam"))
 print(palindrome("Complete the project report"))
